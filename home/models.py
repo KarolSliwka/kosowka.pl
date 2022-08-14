8cf23e2e181ff10bd795ed4e5630a9a7b6ac7a00
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2022-08-14 16:24:20 +0200

    AboutUs and HomeGallery models created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class AboutUs(models.Model):
+    """ Model to display inforamtion in ABOUT US - HOME section """
+
+    main_title = models.CharField(max_length=256, null=True, blank=True)
+    main_image = models.ImageField(
+        upload_to="images/gallery-home", null=True, blank=True)
+    first_title = models.CharField(max_length=256, null=False, blank=True)
+    first_paragraph = models.CharField(
+        max_length=1000, null=False, blank=False)
+    second_title = models.CharField(max_length=256, null=True, blank=True)
+    second_paragraph = models.CharField(
+        max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class HomeGallery(models.Model):
+    """ Model to display few main images in GALLERY HOME section """
+
+    gallery_image = models.ImageField(
+        upload_to="images/gallery-home", null=True, blank=True)
