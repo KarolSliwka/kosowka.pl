ee33a41b20e92ff14668ac1d1d2a93a380926759
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2022-08-14 10:07:25 +0200

    Basic pages created

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+""" Views for Home App"""
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
 
-# Create your views here.
+
+# Main home view
+def home(request):
+    """ A view to display homepage """
+    return render(request, 'home/home.html')
